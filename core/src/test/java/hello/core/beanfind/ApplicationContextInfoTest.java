c7bd4ae74a4dc2f8f3da2ef34f884319871fdd22
kssgit <76714467+kssgit@users.noreply.github.com>
2022-03-29 10:37:32 +0900

    Study 0329
    BeanDefinition을 사용해 Bean의 메타정보 확인하기

--- core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -26,7 +26,7 @@ class ApplicationContextInfoTest {
     void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();//등록된 빈 객체들의 이름들을 반환
         for (String beanDefinitionName : beanDefinitionNames) { // iter + tab -> 이터레이터로 배열,리스트의 for문 자동 완성
-            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);//bean 하나하나에 대한 메타데이터 정
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);//bean 하나하나에 대한 메타데이터 정보
 
             //개발자가 어플을 개발하기위해 등록한 Bean(또는, 외부 라이브러리)
             //여기서는 AppConfig, memberService 등등
